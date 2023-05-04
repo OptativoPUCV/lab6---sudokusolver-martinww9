e7f72979880140cac46bb66749cf61b538e21972
Replit user <>
2023-05-04 23:01:13 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -51,7 +51,21 @@ int is_valid(Node* n){
 
 List* get_adj_nodes(Node* n){
     List* list=createList();
+  int i,j;
+   for(i=0;i<9;i++){
+      for(j=0;j<9;j++){
+         if(n->sudo[i][j]==0){
+            int k;
+            for(k=1;k<=9;k++){
+               Node* new_n=copy(n);
+               new_n->sudo[i][j]=k;
+               pushBack(list,new_n);
+            }
+            return list;
+         }
     return list;
+}
+   }
 }
 
 
